57ef62409091e9ed6bb231277635626d327f0c00
Puskor Roy <puskarkumarroy329@gmail.com>
2025-12-19 10:14:18 +0600

    corsheaders install

--- Phi_mart/settings.py
+++ Phi_mart/settings.py
@@ -45,8 +45,10 @@ INSTALLED_APPS = [
     "whitenoise.runserver_nostatic",
     'django.contrib.staticfiles',
     'rest_framework',
+    "corsheaders",
     'djoser',
     'django_filters',
+    "cloudinary_storage",
     'drf_yasg',
     'api',
     'user',
@@ -58,6 +60,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
@@ -123,6 +126,13 @@ cloudinary.config(
 )
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+   
+]
+
 
 INTERNAL_IPS = [
     # ...
@@ -199,6 +209,9 @@ SIMPLE_JWT = {
 
 
 DJOSER = {
+    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': 'activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': True, 
     'SERIALIZERS': {
         'user_create': "user.serializers.UserCreateSerializer",
         'current_user': 'user.serializers.UserSerializer',
@@ -216,3 +229,17 @@ SWAGGER_SETTINGS = {
       }
    }
 }
+
+
+
+
+
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
